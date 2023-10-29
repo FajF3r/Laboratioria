8998b3a66e6261fcd2c476e4f4f10c01b4ec914d
Maciej Jandzinski <macio.j2004@wp.pl>
2023-10-29 10:11:19 +0100

    first fucntionalities

--- FirstProject/main.c
+++ FirstProject/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+
+    int a;
+    int b;
+    int c;
+
+        int mathOperations(a,b){
+            printf("%d\n",a+b);
+            printf("%d\n",a-b);
+            printf("%d\n",a*b);
+            printf("%d\n",a/b);
+            printf("%d\n",a%b);
+            return 0;
+        }
+
+        void mathBig(int a, int b){
+            if (a>b){
+                printf("%d", a);
+            }else{
+                printf("%d", b);
+                }
+        }
+
+        int mathbigg(int a,int b,int c){
+        if (a>b && a>c){
+            printf("%d", a);
+        }else if(b>a && b>c){
+            printf("%d"c b);
+        }else{
+            printf("%d", c);
+        }
+
+
+int main(){
+
+scanf("%d %d",&a, &b);
+mathbigg(a,b);
+//mathOperations(a,b);
+//mathBig(a,b);
+
 }
