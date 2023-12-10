03ec836cb21ae63366373f006ffe479d338fa50a
FajF3r <Macio.j2004@wp.pl>
2023-12-10 08:41:43 +0100

    ex1 completed

---
+++ lab7/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int glob;
+
+    int addres(int a, int b){
+        printf("%p, %p", &a, &b);
+    }
+
+int main()
+{
+    int local;
+    int *glob = &local;
+    printf("%p %p", &glob,&local);
+    addres(glob,local);
+    return 0;
+}
