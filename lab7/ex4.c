9e59b20144c11eb82db4171af117b1caf982c107
FajF3r <Macio.j2004@wp.pl>
2023-12-10 09:39:20 +0100

    ex4

---
+++ lab7/ex4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int temp;
+int tab[3]={1,5,2};
+int *ptr = &tab[0];
+
+int maxNum(int *ptr,int tab[]){
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        if (tab[i]<tab[i+1])
+        {
+            temp = tab[i+1];
+                        
+        }else if (tab[i]>tab[i+1])
+        {
+            temp = tab[i];
+        }else{
+            i++;
+        }
+        i++;
+        printf("%d\n", temp);
+
+    }
+}
+
+int main()
+{
+    maxNum(*ptr,tab);
+    return 0;
+}
