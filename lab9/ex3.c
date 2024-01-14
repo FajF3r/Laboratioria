6d11387bfec9fdb376185c999256f823aeaf5f2f
FajF3r <Macio.j2004@wp.pl>
2024-01-14 11:32:34 +0100

    ex 3

---
+++ lab9/ex3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+     struct triangle
+     {
+        int a;
+        int b;
+     };
+
+int main()
+{    
+    struct triangle a1;
+    a1.a = 5;
+    a1.b = 4;
+    struct triangle a2;
+    a2.a = 3;
+    a2.b = 2;
+    struct triangle a3;
+    a3.a = 1;
+    a3.b = 1;
+
+    
+}
